faa01a1fafcb809369e3cb0e52059c05ccc03cac
Brymer Meneses <meneses.brymer@gmail.com>
2021-07-11 20:17:17 +0800

    improve launching of workspace

--- src/launch-dev-workspace.sh
+++ src/launch-dev-workspace.sh
@@ -10,11 +10,12 @@ source $CONDA_INIT
 conda activate gdetect
 
 # Launch another kitty instance and run the FRONTEND server
-kitty --directory="${PROJECT_DIR}/src/frontend" yarn start &
+setsid kitty --directory="${PROJECT_DIR}/src/frontend" yarn start &
 
 # Launch Neovim
 cd $PROJECT_DIR
-kitty --directory="${PROJECT_DIR}/src" nvim &
+setsid kitty --directory="${PROJECT_DIR}/src" nvim &
+disown
 
 # Run the BACKEND server
 cd $PROJECT_DIR/src/backend

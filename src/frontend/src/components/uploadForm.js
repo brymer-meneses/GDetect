82f986811a8daa4359b9ad82d9b17b2a953483af
Brymer Meneses <meneses.brymer@gmail.com>
2021-07-12 19:07:48 +0800

    feat: add steps

--- src/frontend/src/components/uploadForm.js
+++ src/frontend/src/components/uploadForm.js
@@ -6,16 +6,28 @@ import { Button, message } from 'antd';
 import '../styles/uploadForm.css';
 import messageHandler from './message';
 import TextInput from './textInput';
-import { MailOutlined, UserOutlined } from '@ant-design/icons';
+import {
+  MailOutlined,
+  UserOutlined,
+  SmileOutlined,
+  LoadingOutlined,
+  SolutionOutlined,
+} from '@ant-design/icons';
+
+import { Steps } from 'antd';
+
+import useWindowSize from '../utils/useWindowSize';
 
 const API_LINK = 'http://127.0.0.1:8000/api/upload-images';
 
 // TODO
-// - Make the UI responsive
-// - Add Steps
 // - Change message to alert antdesign
 
+const { Step } = Steps;
+
 function UploadForm() {
+  const { width } = useWindowSize();
+
   const [selfieImage, setSelfieImage] = useState(null);
   const [idImage, setIdImage] = useState(null);
 
@@ -29,6 +41,8 @@ function UploadForm() {
 
   const [email, setEmail] = useState(null);
   const [fullName, setFullName] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+  const [isProcessDone, setIsProcessDone] = useState(false);
 
   const fileUploadHandler = () => {
     const formData = new FormData();
@@ -36,6 +50,7 @@ function UploadForm() {
     formData.append('files', selfieImage);
     formData.append('files', idImage);
     formData.append('userInfo', { fullName, email });
+    setIsUploading(true);
 
     const key = 'updatable';
     message.loading({ content: 'Uploading Images...', key });
@@ -43,10 +58,12 @@ function UploadForm() {
       .post(API_LINK, formData)
       .then((res) => {
         messageHandler(res, key);
+        setIsProcessDone(true);
       })
       .catch((err) => {
         if (err.response !== null) {
           messageHandler(err.response, key);
+          setIsProcessDone(true);
         } else {
           console.log(err);
         }
@@ -85,8 +102,29 @@ function UploadForm() {
         disabled={isUploadButtonDisabled}
         onClick={fileUploadHandler}
       >
-        Upload Information
+        Submit
       </Button>
+      <Steps
+        style={{ marginTop: '4rem', marginLeft: '1rem', marginRight: '1rem' }}
+        direction={width < 800 ? 'vertical' : 'horizontal'}
+      >
+        <Step
+          status={isUploadButtonDisabled ? 'wait' : 'finish'}
+          title="Upload Information"
+          description="Fill out your email address as well as your full name and upload your pictures"
+        />
+        <Step
+          status={isUploading ? 'finish' : 'wait'}
+          title="Click Submit"
+          description="After completing the required information, click the submit button to proceed."
+          icon={isUploading && !isProcessDone ? <LoadingOutlined /> : null}
+        />
+        <Step
+          status={isProcessDone ? 'finish' : 'wait'}
+          title="Wait for Verification"
+          description="After submitting your information,  wait for your identity to be verified"
+        />
+      </Steps>
     </div>
   );
 }
